6d6c8e75542134484db25accc8d9f403c029c748
JInfantesC <jose.infantes@protonmail.com>
2017-07-16 16:48:53 +0200

    Added Accounts

--- client/main.js
+++ client/main.js
@@ -52,26 +52,6 @@ Template.resolution.events({
         Resolutions.remove(this._id);//this means clicked resolution.
     }
 });
-
-
-
-
-/* FRON END CODE
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Accounts.ui.config({
+    passwordSignupFields:"USERNAME_ONLY"
 });
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-*/
